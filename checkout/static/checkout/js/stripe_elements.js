ced51a7c671b4ce42b7e3ca5dfb6d8949fe978bf
ernestasambrozas <ernestasambrozas@gmail.com>
2023-12-09 15:44:37 -0500

    Added stripe elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,19 @@
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from .forms import OrderForm
-from cart.contexts import cart_contents  # Import the cart_contents function
+from cart.contexts import cart_contents
+from django.conf import settings
 
 def checkout(request):
-    # Debugging: Print the current session cart
-    print("Session Cart:", request.session.get('cart', {}))
-
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment")
@@ -14,20 +12,14 @@ def checkout(request):
 
     order_form = OrderForm()
     
-    # Manually get cart contents
     cart_context = cart_contents(request)
-
-    # Debugging: Print the cart context
-    print("Cart Context:", cart_context)
 
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'cart_items': cart_context['cart_items'],  # Add cart items to context
-        'total_after_discount': cart_context['total_after_discount'],  # Add total after discount to context
+        'cart_items': cart_context['cart_items'],
+        'total_after_discount': cart_context['total_after_discount'],
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
     }
-
-    # Debugging: Print the final context being passed to the template
-    print("Context being passed to template:", context)
 
     return render(request, template, context)
